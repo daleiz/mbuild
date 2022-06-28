61f149c017e6f9f6f4fe9276765d830f5923323d
daleiz <30970925+daleiz@users.noreply.github.com>
2022-06-28 12:21:32 +0800

    support multiple targets in a rule

--- lib/rule.ml
+++ lib/rule.ml
@@ -11,6 +11,16 @@ let create ?(deps = []) ?(cmds = []) target =
   let f name is_phony = { target = name; is_phony; deps; cmds } in
   match target with Phony name -> f name true | File name -> f name false
 
+let string_of_target t = match t with Phony s -> s | File s -> s
+
+let create_m ?(deps = []) ?(cmds = []) targets =
+  let t0 = List.hd targets in
+  let tl = List.tl targets in
+  let r0 = create ~deps ~cmds t0 in
+  let f t = create ~deps:[ string_of_target t0 ] ~cmds:[ ":" ] t in
+  let rs = List.map f tl in
+  List.append [ r0 ] rs
+
 let target t = t.target
 let is_target_phony t = t.is_phony
 let deps t = t.deps
--- lib/rule.mli
+++ lib/rule.mli
@@ -2,6 +2,7 @@ type t
 type target = Phony of string | File of string
 
 val create : ?deps:string list -> ?cmds:string list -> target -> t
+val create_m : ?deps:string list -> ?cmds:string list -> target list -> t list
 val target : t -> string
 val is_target_phony : t -> bool
 val deps : t -> string list
